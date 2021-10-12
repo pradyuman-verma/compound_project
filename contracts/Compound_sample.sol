916cb56a054d80102efdd308a3b8ae3f7a8c1b5a
pradyuman-verma <pradyumnverma27@gmail.com>
2021-10-12 19:36:44 +0530

    update to borrow function

--- contracts/Compound_sample.sol
+++ contracts/Compound_sample.sol
@@ -9,26 +9,47 @@ import "./interfaces/compound.sol";
 contract CompoundSample {
     event MyLog(string, uint256);
 
-    mapping(address => uint256) addressToToken;
+    mapping(address => uint256) addressTocEth;
 
-    uint256 cTokenBalance = 0;
     uint256 cEthBalance = 0;
 
-    function supplyEthToCompound(address payable _cEtherContract)
-        public
-        payable
-        returns (bool)
-    {
+    function supplyEthToCompound(
+        address payable _cEtherContract,
+        bool supplyType
+    ) public payable returns (bool) {
         // Creating a reference to the corresponding cToken contract
         CEth cToken = CEth(_cEtherContract);
         cToken.mint{value: msg.value}();
         // Sending ctoken to msg.sender
-        addressToToken[msg.sender] =
+        addressTocEth[msg.sender] =
             cToken.balanceOf(address(this)) -
-            cTokenBalance;
-        cTokenBalance = cToken.balanceOf(address(this));
-        cToken.transfer(msg.sender, cToken.balanceOf(address(this)));
+            cEthBalance;
+        cEthBalance = cToken.balanceOf(address(this));
+        if (supplyType) cToken.transfer(msg.sender, addressTocEth[msg.sender]);
         return true;
+    }
+
+    function withdrawEth(uint256 amount, address _cEtherContract)
+        public
+        returns (bool)
+    {
+        // Creating a reference to the corresponding cToken contract
+        CEth cToken = CEth(_cEtherContract);
+
+        uint256 redeemResult;
+
+        // transfering ctoken from user to our contract address.
+        cToken.transferFrom(msg.sender, address(this), amount);
+        if (addressTocEth[msg.sender] != 0) {
+            addressTocEth[msg.sender] = addressTocEth[msg.sender] - amount;
+        }
+        cToken.approve(_cEtherContract, amount);
+        redeemResult = cToken.redeem(amount);
+
+        emit MyLog("If this is not 0, there was an error", redeemResult);
+
+        (bool sent, ) = msg.sender.call{value: address(this).balance}("");
+        return sent;
     }
 
     function supplyErc20ToCompound(
@@ -54,8 +75,7 @@ contract CompoundSample {
     }
 
     function withdrawErc20Tokens(
-        uint256 amount,
-        bool redeemType,
+        uint256 _amount,
         address _cErc20Contract,
         address _erc20Contract
     ) public returns (bool) {
@@ -67,9 +87,9 @@ contract CompoundSample {
         uint256 redeemResult;
 
         // transfering ctoken from user to our contract address.
-        cToken.transferFrom(msg.sender, address(this), amount);
-        cToken.approve(_cErc20Contract, amount);
-        redeemResult = cToken.redeem(amount);
+        cToken.transferFrom(msg.sender, address(this), _amount);
+        cToken.approve(_cErc20Contract, _amount);
+        redeemResult = cToken.redeem(_amount);
 
         emit MyLog("If this is not 0, there was an error", redeemResult);
         // transfering erc20 to msg.sender
@@ -78,65 +98,43 @@ contract CompoundSample {
         return true;
     }
 
-    function withdrawEth(
-        uint256 amount,
-        bool redeemType,
-        address _cEtherContract
-    ) public returns (bool) {
-        // Creating a reference to the corresponding cToken contract
-        CEth cToken = CEth(_cEtherContract);
-
-        uint256 redeemResult;
-
-        // transfering ctoken from user to our contract address.
-        cToken.transferFrom(msg.sender, address(this), amount);
-        cToken.approve(_cEtherContract, amount);
-        redeemResult = cToken.redeem(amount);
-
-        emit MyLog("If this is not 0, there was an error", redeemResult);
-
-        (bool sent, ) = msg.sender.call{value: address(this).balance}("");
-        return sent;
-    }
-
     function() external payable recieve;
 
     function borrowEth(
         address payable _cEtherAddress,
-        address _comptrollerAddress,
         address _cTokenAddress,
-        uint256 _amount
-    ) public payable returns (uint256) {
+        uint256 _amount,
+        address _underlyingAddress,
+        uint256 _underlyingToSupplyAsCollateral
+    ) public payable returns (bool) {
+        uint256 amount = _amount;
         CEth cEth = CEth(_cEtherAddress);
         CErc20 cToken = CErc20(_cTokenAddress);
+        Erc20 underlying = Erc20(_underlyingAddress);
 
-        Comptroller comptroller = Comptroller(_comptrollerAddress);
+        Comptroller comptroller = Comptroller(
+            0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B
+        );
         PriceFeed priceFeed = PriceFeed(
             0x922018674c12a7F0D394ebEEf9B58F186CdE13c1
         );
 
         uint256 price = priceFeed.getUnderlyingPrice(_cEtherAddress);
+        underlying.approve(address(this), _underlyingToSupplyAsCollateral);
+        underlying.transferFrom(
+            msg.sender,
+            address(this),
+            _underlyingToSupplyAsCollateral
+        );
 
-        if (addressToToken[msg.sender] == 0) {
-            // If user haven't used supplyEth function then use this.
-            // Supply underlying as collateral, get cToken in return
-            uint256 error = cToken.mint(msg.value);
-            require(error == 0, "CErc20.mint Error");
-            addressToToken[msg.sender] =
-                cToken.balanceOf(address(this)) -
-                cTokenBalance;
-            cTokenBalance = cToken.balanceOf(address(this));
-        } else {
-            // else user have some ctoken in its address, then user have to send those ctoken
-            // to our contract address
-            // Approve transfer of underlying
-            cToken.approve(_cTokenAddress, addressToToken[msg.sender]);
-            cToken.transferFrom(msg.sender, address(this), _amount);
-            addressToToken[msg.sender] =
-                cToken.balanceOf(address(this)) -
-                cTokenBalance;
-            cTokenBalance = cToken.balanceOf(address(this));
-        }
+        uint256 error = cToken.mint(_underlyingToSupplyAsCollateral);
+        require(error == 0, "CErc20.mint Error");
+
+        addressTocEth[msg.sender] +=
+            cToken.balanceOf(address(this)) -
+            cEthBalance;
+
+        cEthBalance = cToken.balanceOf(address(this));
 
         // Enter the market so you can borrow another type of asset
         address[] memory cTokens = new address[](1);
@@ -153,24 +151,21 @@ contract CompoundSample {
 
         // calculating maximum borrow:
         uint256 maxBorrow = (liquidity * (10**18)) / price;
-        require(maxBorrow > _amount, "Can't borrow this much!");
+        require(maxBorrow > amount, "Can't borrow this much!");
 
         // Borrow, then check the underlying balance for this contract's address
-        cEth.borrow(_amount);
-
-        uint256 borrows = cEth.borrowBalanceCurrent(address(this));
-
-        emit MyLog("Current ETH borrow amount", borrows);
-
-        (bool sent, ) = msg.sender.call{value: borrows}("");
-        return borrows;
+        cEth.borrow(amount);
+        (bool sent, ) = msg.sender.call{value: cEth.balanceOf(address(this))}(
+            ""
+        );
+        return sent;
     }
 
     function EthRepayBorrow(address _cEtherAddress) public returns (bool) {
         CEth cEth = CEth(_cEtherAddress);
-        uint256 amount = addressToToken[msg.sender];
-        addressToToken[msg.sender] = 0;
-        cTokenBalance = cTokenBalance - amount;
+        uint256 amount = addressTocEth[msg.sender];
+        addressTocEth[msg.sender] = 0;
+        cEthBalance = cEthBalance - amount;
         cEth.repayBorrow{value: amount}();
         (bool sent, ) = msg.sender.call{value: cEth.balanceOf(address(this))}(
             ""
