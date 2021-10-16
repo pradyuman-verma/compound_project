354d4ad3b958b84b2720226867b77521d296ba4a
pradyuman-verma <pradyumnverma27@gmail.com>
2021-10-16 13:22:48 +0530

    update for adding test

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,8 +13,8 @@ module.exports = {
   networks:{
     hardhat:{
       forking:{
-        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_ENDPOINT}`
-        //blockNumber: 13405033
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_ENDPOINT}`,
+        blockNumber: 13405033
       }
     }
   }
